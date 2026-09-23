3327cbffce9a37aee8be6f66fa39e96f0069e7ce
ygarciaar <ygarciaar@cesde.net>
2026-09-22 20:49:42 -0500

    nueva actualizaicion

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+const URL_BASE = "https://api-ofbo.onrender.com/"
+
+
+export let end_points = {
+    estudiantes: URL_BASE + "estudiantes/",
+    cursos: URL_BASE + "cursos/",
+    matriculas: URL_BASE + "matriculas/",
+}
---
+++ src/api/services.js
@@ -0,0 +1,17 @@
+import { end_points } from "./config";
+
+export function getData(){
+    fetch()
+}
+
+export function createData(){
+    fetch()
+}
+
+export function updateData(){
+    fetch()
+}
+
+export function deleteData(){
+    fetch()
+}
